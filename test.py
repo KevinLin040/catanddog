014ec9d6ca381112e560c5e8745f9360b8e736ad
Kevin Lin <kevin040@hotmail.com>
2020-10-19 00:00:20 +0800

    txt

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ class_indices = {'Cat':0,'Dog':1}
 inverse_dict = dict((val, key) for key, val in class_indices.items()) 
 
 #載入圖片並處理
-img = cv2.imread('31.jpg')
+img = cv2.imread('32.jpg')
 img2 = img.copy()
 img2 = cv2.resize(img2,(224,224))
 img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2RGB)
@@ -24,7 +24,7 @@ predict_class = np.argmax(result)
 #預測結果印在圖片上
 txt = str(inverse_dict[int(predict_class)]) + ' ' +str(round((result[predict_class]*100),1)) + '%'
 print(txt)
-img = cv2.putText(img,txt,(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
+img = cv2.putText(img,txt,(10,30),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
 #显示图片
 cv2.imshow('img',img)
 cv2.waitKey(0)
